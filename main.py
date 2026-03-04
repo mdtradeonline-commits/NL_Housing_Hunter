a18338b9ebe151fd1146e285a9f785359c4bd337
mdtradeonline-commits <mdtrade.online@gmail.com>
2026-03-04 19:02:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,23 @@ async def start_cmd(message: types.Message):
     )
 
 @dp.message_handler(lambda message: message.text in ['🇷🇺 Русский', '🇬🇧 English', '🇳🇱 Nederlands'])
+async @dp.message_handler(lambda message: message.text in ['🇷🇺 Русский', '🇬🇧 English', '🇳🇱 Nederlands'])
 async def set_lang(message: types.Message):
+    user_id = message.from_id
+    lang_choice = message.text
+    
+    # Запоминаем выбор в нашу "базу"
+    if user_id in users_db:
+        users_db[user_id]['lang'] = lang_choice
+    
+    # Ответ в зависимости от выбора
+    responses = {
+        '🇷🇺 Русский': "Отлично! Теперь я буду присылать тебе варианты жилья на русском. Твои 24 часа бесплатного поиска начались! 🏠",
+        '🇬🇧 English': "Great! From now on, I will send you housing options in English. Your 24-hour free trial has started! 🏠",
+        '🇳🇱 Nederlands': "Geweldig! Vanaf nu stuur ik je woningopties in het Nederlands. Je gratis proefperiode van 24 uur is begonnen! 🏠"
+    }
+    
+    await message.answer(responses[lang_choice], reply_markup=types.ReplyKeyboardRemove())
     lang = message.text
     # Тут мы в будущем будем менять язык всех сообщений
     await message.answer(f"Success! / Принято! / Succes!\nSelected: {lang}", reply_markup=types.ReplyKeyboardRemove())
